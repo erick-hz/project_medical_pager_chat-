8a76035177bfa17597853f1d53a9d48407a43a15
Erick Hernandez <yerickk8@gmail.com>
2023-03-27 10:05:57 -0600

    Setup Front end

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+const apiKey = "mcc8sx5gncyk";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
