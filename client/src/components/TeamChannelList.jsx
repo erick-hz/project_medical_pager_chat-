2dd7c1afc9a5ac9daff33c784666d3430b24a7c1
Erick Hernandez <yerickk8@gmail.com>
2023-04-04 11:36:00 -0600

    Initial Structure

--- client/src/components/TeamChannelList.jsx
+++ client/src/components/TeamChannelList.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React from "react";
 import { AddChannel } from "../assets";
 
-const TeamChannelList = ({ setToggleContainer, children, error = false, loading, type, isCreating, setIsCreating, setCreateType, setIsEditing }) => {
-  if(error) {
-    return type === 'team' ? (
+const TeamChannelList = ({
+  setToggleContainer,
+  children,
+  error = false,
+  loading,
+  type,
+  isCreating,
+  setIsCreating,
+  setCreateType,
+  setIsEditing,
+}) => {
+  if (error) {
+    return type === "team" ? (
       <div className="team-channel-list">
-                <p className="team-channel-list__message">
-                    Connection error, please wait a moment and try again.
-                </p>
-            </div>
-    ) : null
+        <p className="team-channel-list__message">
+          Connection error, please wait a moment and try again.
+        </p>
+      </div>
+    ) : null;
   }
 
-  if(loading) {
+  if (loading) {
     return (
       <div className="team-channel-list">
         <p className="team-channel-list__message loading">
@@ -40,6 +50,6 @@ const TeamChannelList = ({ setToggleContainer, children, error = false, loading,
       {children}
     </div>
   );
-}
+};
 
-export default TeamChannelList
+export default TeamChannelList;
